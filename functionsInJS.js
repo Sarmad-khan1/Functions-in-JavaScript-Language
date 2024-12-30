d08b492e44bbfe1f55cbb3670145966648c2629e
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-12-30 14:20:10 +0500

    Add files via upload

---
+++ functionsInJS.js
@@ -0,0 +1,23 @@
+//Creating Functions In JavaScript
+
+	<script>
+        function abcc(num1,num2){
+            console.log(num1*num2);
+        }
+        abcc(5,50);
+
+
+        function sum(n1,n2){
+            console.log(n1+n2);
+        }
+        sum(1,28);
+
+
+
+        function divide(numb1,numb2){
+            console.log(numb1/numb2);
+        }
+        divide(10,20);
+
+        
+    </script>
